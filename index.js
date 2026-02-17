16fedb86c227ad313a5411e87338fee28643cfdd
Assignment-1 <sayoidrasel80@gmail.com>
2026-02-17 19:28:14 +0600

    html commit

---
+++ index.js
@@ -0,0 +1,8 @@
+ <div class="card bg-base-100 w-[350px] p-3 shadow-md">
+            <a class="text-blue-500 p-3 shadow-md w-3/12 rounded-lg ml-6" href=""><i class="fa-solid fa-truck"></i></a>
+        <div class="card-body">
+            <h2 class="card-title">First Delivery</h2>
+            <p>Get your orders delivery to yours doorstep quickly and efficiently.</p>
+        <div class="card-actions justify-end">
+    </div>
+</div>
